e174870a3901f1e648733216c5f5deafd437a606
Ray Mondragon <raymondragonn@gmail.com>
2024-10-27 21:26:38 -0600

    implementación PWA

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Registro de SW exitoso', reg))
+      .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+
 const bottoms = document.querySelectorAll('.bottom')
 let arr1 = [];
 let arr2 = [];
